33f13c8d526a33955b155060fc0407c4179094f6
ese07 <jjordaa@uoc.edu>
2025-02-23 00:16:09 +0100

    Nueva

--- js/app.js
+++ js/app.js
@@ -12,11 +12,10 @@ const error=document.getElementById("error");
 //Funciones de evento
 function comprobarForm(event) {
     //Comprobar cambios
-    if (nickInput.value.length==0) {
-        console.log("No hay nick");
+    if (nickInput.value.match(/(?<!\S)[0-9]/)) {
         nickInput.focus();
         event.preventDefault();
-        error.innerText="El campo de nick no puede estar vacío";
+        error.innerText="El campo de nick no puede comenzar por un número";
         return false;
     }else if (tamanoInput.value=="0") {
         console.log("No se ha seleccionado tamaño de panel");
