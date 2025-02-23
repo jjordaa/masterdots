9e38cfc2827c0fb139709acc349ce98eb49a0d08
ese07 <jjordaa@uoc.edu>
2025-02-23 14:49:51 +0100

    Nuevo

--- js/datosUsuario.js
+++ js/datosUsuario.js
@@ -2,21 +2,33 @@
 * JS Para la gestión de los datos de usuario
 *
 */
+var nick;
+var date; 
 
+//SessionStorage
 function datosUsuario(nick){                //Función para guardar el nick en el sessionStorage
     sessionStorage.setItem('nick',nick.value);
 }
 
 function getDatosUsuario(){                 //Función para obtener el nick del sessionStorage
-    let nick = sessionStorage.getItem('nick');
+    nick = sessionStorage.getItem('nick');
     console.log(nick);    
 }
 //Funcion para comprobar de los datos de usuario no está vacio antes de ir a jugar 
 function comprobacionDatosUsuario(){
-    let nick = getDatosUsuario();
+    nick = getDatosUsuario();
     if (nick==null) {
         sessionStorage.setItem('error','No se ha rellenado correctamente el formulario');
         return false;        
         } 
     return true;    
+}
+
+//localStorage
+function historicoUsuarios(nick){                //Función para guardar el nick en el localStorage
+    let registroUsuario = {
+        usuario:nick.value,
+        fecha: new Date.now()
+    }
+    localStorage.setItem('historico',JSON.stringify(registroUsuario));
 }
